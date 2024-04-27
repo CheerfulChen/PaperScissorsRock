3e66a2d8ce5afbac78a0132ab2a30ac321fe4942
CheerfulChen <Cheerful_Chen5@163.com>
2024-04-27 15:42:54 +0800

    Initial Commit

---
+++ PaperScissorsRock/PaperScissorsRockApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperScissorsRockApp.swift
+//  PaperScissorsRock
+//
+//  Created by 颜宇辰 on 2024/4/27.
+//
+
+import SwiftUI
+
+@main
+struct PaperScissorsRockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
